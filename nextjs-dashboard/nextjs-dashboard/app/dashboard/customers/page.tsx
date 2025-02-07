5cecd44ca86e05a871ed041933568dc2d008bc44
kavyagali-github <kavyagali17@gmail.com>
2025-02-06 19:56:24 -0500

    LAB 5

---
+++ nextjs-dashboard/nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,15 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
+// import { lusitana } from './ui/fonts';
+import Image from 'next/image';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <p>This is Customer Page</p>
+      <p>Nested in the dashboard route</p>
+    </main>
+  );
+}
